0b5b700012b1fc8bc513768d43b8aaaeb6f40163
Tsvetelina Vatashka <tsvetelina.vatashka@skrill.com>
2015-01-09 17:55:33 +0200

    Changes in Enumerations

--- src/main/java/api/application/request/Gender.java
+++ src/main/java/api/application/request/Gender.java
@@ -1,5 +1,44 @@
 package api.application.request;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+@XmlRootElement
+@XmlJavaTypeAdapter(Gender.XmlTypeAdapter.class)
 public enum Gender {
-    Male, Female;
+
+    MALE("male"),
+    FEMALE("female");
+
+    public static class XmlTypeAdapter extends XmlAdapter<String, Gender> {
+        @Override
+        public Gender unmarshal(String enumName) {
+            return Gender.fromString(enumName);
+        }
+
+        @Override
+        public String marshal(Gender enumName) {
+            return enumName.getType();
+        }
+    }
+
+    private String gender;
+
+    private Gender(String contentType) {
+        this.gender = contentType;
+    }
+
+    public static Gender fromString(String gender) {
+        for (Gender current : values()) {
+            if (current.gender.equalsIgnoreCase(gender)) {
+                return current;
+            }
+        }
+        throw new IllegalArgumentException("Unsupported gender: " + gender);
+    }
+
+    public String getType() {
+        return gender;
+    }
 }
--- src/main/java/api/application/request/Status.java
+++ src/main/java/api/application/request/Status.java
@@ -1,11 +1,35 @@
 package api.application.request;
 
 public enum Status {
-    
-    NOT_CHECKED,
-    APPROVED,
-    REJECTED,
-    BLOCKED,
-    INTERVIEW;
+
+    PENDING("Not checked", 0),
+    APPROVED("Approved candidate", 1),
+    REJECTED("Rejected candidate", -1),
+    BLOCKED("Blocked candidate", -2),
+    INTERVIEW("Interview appointmet", 2);
+
+    private String message;
+    private int status;
+
+    private Status(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 
 }
